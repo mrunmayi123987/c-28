564079e1c40bf41bc4bf49ebca517fad7123ab93
mrunmayi123987 <mrunmayisshendkar@gmail.com>
2021-02-08 19:44:32 +0530

    change file

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,8 +39,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new Slingshot(bird.body,{x:200,y:200});
 }
 
 function draw(){
@@ -63,6 +64,18 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+
+    Matter.Body.setPosition(bird.body,{x:mouseX , y:mouseY});
+
+}
+
+function mouseReleased(){
+
+     slingshot.fly();
+
 }
